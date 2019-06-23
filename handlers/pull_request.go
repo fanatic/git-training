2855ac6d1f98aa9865c802f4cedf9d0b1312bc51
Jason Parrott <fanatic+github@gmail.com>
2019-06-23 13:12:48 -0400

    Step 6

--- handlers/pull_request.go
+++ handlers/pull_request.go
@@ -28,11 +28,14 @@ func (h *PullRequestHandler) Handle(ctx context.Context, eventType, deliveryID s
 
 	logrus.Infof("Handling %s", event.GetAction())
 	switch event.GetAction() {
-	case "opened":
+	case "opened", "reopened":
 		if err := h.opened(ctx, event); err != nil {
 			return errors.Wrap(err, "failed to parse pr")
 		}
-
+	case "edited":
+		if err := h.edited(ctx, event); err != nil {
+			return errors.Wrap(err, "failed to parse pr")
+		}
 	}
 
 	return nil
@@ -79,4 +82,64 @@ Now let's link it to our issue so that when the PR is merged, GitHub will automa
 	}
 
 	return nil
+}
+
+func (h *PullRequestHandler) edited(ctx context.Context, event github.PullRequestEvent) error {
+	installationID := githubapp.GetInstallationIDFromEvent(&event)
+	client, err := h.NewInstallationClient(installationID)
+	if err != nil {
+		return err
+	}
+
+	repo := event.GetRepo()
+	repoOwner := repo.GetOwner().GetLogin()
+	repoName := repo.GetName()
+	author := event.GetSender()
+	prNumber := event.GetPullRequest().GetNumber()
+
+	issueNumber, err := FindIssueNumberByAssignee(ctx, client, repoOwner, repoName, author.GetLogin())
+	if err != nil {
+		return err
+	} else if issueNumber == 0 {
+		return nil
+	}
+
+	// TODO confirm issue linked
+
+	// TODO create this comment as a review instead of a comment
+
+	review := github.PullRequestReviewRequest{
+		Event: String("REQUEST_CHANGES"),
+		Body: String(fmt.Sprintf(`## Step 6: Respond to a review
+
+Your pull request is looking great!
+
+Let’s add some content to your file. Replace the contents of your file with a quotation or meme or witty comment. 
+
+### :keyboard: Activity: Change your file
+
+1. Click the [Files Changed tab](https://github.factset.com/%s/%s/pull/%d/files) in this pull request
+1. Click on the pencil icon found on the right side of the screen to edit your newly added file
+1. Replace line 1 with something new
+1. Scroll to the bottom and click **Commit Changes**
+
+<hr>
+<h3 align="center">I'll respond when I detect a commit on this branch.</h3>`, repoOwner, repoName, prNumber)),
+		Comments: []*github.DraftReviewComment{
+			&github.DraftReviewComment{
+				Path:     String("users/" + author.GetLogin() + ".md"),
+				Position: Int(1),
+				Body:     String("Replace this with a quotation or meme or witty comment"),
+			},
+		},
+	}
+	if _, _, err := client.PullRequests.CreateReview(ctx, repoOwner, repoName, prNumber, &review); err != nil {
+		logrus.WithError(err).Error("Failed to create pr review")
+	}
+
+	return nil
+}
+
+func Int(i int) *int {
+	return &i
 }
