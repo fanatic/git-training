7b5a144b639b94b4af79f91bf04e4cd9745da31d
Jason Parrott <fanatic+github@gmail.com>
2019-06-23 11:55:45 -0400

    Step 2

--- handlers/issues.go
+++ handlers/issues.go
@@ -32,6 +32,11 @@ func (h *IssuesHandler) Handle(ctx context.Context, eventType, deliveryID string
 		if err := h.opened(ctx, event); err != nil {
 			return errors.Wrap(err, "failed to parse issue open")
 		}
+	case "assigned":
+		logrus.Infof("Handling %s", event.GetAction())
+		if err := h.assigned(ctx, event); err != nil {
+			return errors.Wrap(err, "failed to parse issue open")
+		}
 	default:
 		logrus.Infof("Handling %s", event.GetAction())
 	}
@@ -54,13 +59,9 @@ func (h *IssuesHandler) opened(ctx context.Context, event github.IssuesEvent) er
 	comment := github.IssueComment{
 		Body: String(fmt.Sprintf(`# :wave: Welcome to GitHub Training, @%s!
 
-To get started, I’ll guide you through some important first steps in coding and collaborating on GitHub.
-
-### Using issues
+I’ll guide you through some important first steps in coding and collaborating on GitHub.
 
 This is an issue <sup>[:book:](https://help.github.com/articles/github-glossary/#issue)</sup>: a place to record bugs, request enhancements, or answer questions about your repo.
-
-Issue titles are like email subject lines. They tell your collaborators what the issue is about at a glance. 
 
 <hr>
 <h3 align="center">Keep reading below to find your first task</h3>`, author.GetLogin())),
@@ -81,7 +82,7 @@ Unassigned issues don't have owners to look after them.
 <hr>
 <h3 align="center">I'll respond when I detect you've assigned yourself to this issue.</h3>
 
-> If you perform an expected action and don't see a response from me, wait a few seconds and refresh the page for your next steps._`)),
+> If you perform an expected action and don't see a response from me, wait a few seconds and refresh the page for your next steps.`)),
 	}
 	if _, _, err := client.Issues.CreateComment(ctx, repoOwner, repoName, issueNumber, &comment); err != nil {
 		logrus.WithError(err).Error("Failed to create issue comment 2")
@@ -92,4 +93,61 @@ Unassigned issues don't have owners to look after them.
 
 func String(s string) *string {
 	return &s
+}
+
+func (h *IssuesHandler) assigned(ctx context.Context, event github.IssuesEvent) error {
+	installationID := githubapp.GetInstallationIDFromEvent(&event)
+	client, err := h.NewInstallationClient(installationID)
+	if err != nil {
+		return err
+	}
+
+	repo := event.GetRepo()
+	issueNumber := event.GetIssue().GetNumber()
+	repoOwner := repo.GetOwner().GetLogin()
+	repoName := repo.GetName()
+	author := event.GetIssue().GetUser()
+	assignee := event.GetIssue().GetAssignee()
+
+	if author.GetLogin() != assignee.GetLogin() {
+		return nil
+	}
+
+	comment := github.IssueComment{
+		Body: String(fmt.Sprintf(`## Introduction to a typical workflow
+
+Now that you're familiar with issues, let's use this issue to track your path to your first contribution.
+
+People use different workflows to contribute to software projects, but the simplest and most effective way to contribute on GitHub is the GitHub flow.
+
+:tv: [Video: Understanding the GitHub flow](https://www.youtube.com/watch?v=PBI2Rz-ZOxU)
+
+<hr>
+<h3 align="center">Read below for next steps</h3>`)),
+	}
+	if _, _, err := client.Issues.CreateComment(ctx, repoOwner, repoName, issueNumber, &comment); err != nil {
+		logrus.WithError(err).Error("Failed to create issue comment")
+	}
+
+	comment = github.IssueComment{
+		Body: String(fmt.Sprintf(`## Step 2: Create a branch
+
+Let’s complete the first step of the GitHub flow: creating a branch <sup>[:book:](https://help.github.com/articles/github-glossary/#branch)</sup>.
+
+### :keyboard: Activity: Your first branch
+
+1. Navigate to the [Code tab](https://github.factset.com/%s/%s)
+2. Click **Branch: master** in the drop-down
+3. In the field, enter a name for your branch, like "feat/username-1"
+4. Click **Create branch: <name>** or press the “Enter” key to create your branch
+
+
+<hr>
+<h3 align="center">I'll respond when I detect a new branch has been created in this repository.</h3>`, repoOwner, repoName)),
+	}
+	if _, _, err := client.Issues.CreateComment(ctx, repoOwner, repoName, issueNumber, &comment); err != nil {
+		logrus.WithError(err).Error("Failed to create issue comment 2")
+	}
+
+	return nil
 }
